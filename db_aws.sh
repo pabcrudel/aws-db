9049f7130c565af7f9db7508d489a3dace0bf769
m1bore <122971250+m1bore@users.noreply.github.com>
2023-11-17 18:47:09 +0100

    Update 3 db_aws.sh

--- db_aws.sh
+++ db_aws.sh
@@ -10,25 +10,6 @@ DB_INSTANCE_CLASS="db.t2.micro"
 DB_ALLOCATED_STORAGE=20
 DB_PORT=3306
 
-# Crear instancia de base de datos RDS
-aws rds create-db-instance \
-    --db-instance-identifier $DB_INSTANCE_IDENTIFIER \
-    --db-instance-class $DB_INSTANCE_CLASS \
-    --engine $DB_ENGINE \
-    --master-username $DB_MASTER_USERNAME \
-    --master-user-password $DB_MASTER_PASSWORD \
-    --allocated-storage $DB_ALLOCATED_STORAGE \
-    --db-name $DB_NAME \
-    --port $DB_PORT
-
-# Esperar hasta que la instancia esté disponible
-echo "Esperando a que la instancia de la base de datos esté disponible..."
-aws rds wait db-instance-available --db-instance-identifier $DB_INSTANCE_IDENTIFIER
-echo "La instancia de la base de datos está disponible."
-
-# Obtener el endpoint de la instancia de la base de datos
-DB_ENDPOINT=$(aws rds describe-db-instances --db-instance-identifier $DB_INSTANCE_IDENTIFIER --query "DBInstances[0].Endpoint.Address" --output text)
-
 # Crear grupo de seguridad para permitir la conexión desde MySQL Workbench
 aws ec2 create-security-group \
     --group-name mysql-workbench-sg \
@@ -43,6 +24,27 @@ aws ec2 authorize-security-group-ingress \
     --protocol tcp \
     --port $DB_PORT \
     --source-cidr 0.0.0.0/0
+# Crear instancia de base de datos RDS
+aws rds create-db-instance \
+    --db-instance-identifier $DB_INSTANCE_IDENTIFIER \
+    --db-instance-class $DB_INSTANCE_CLASS \
+    --engine $DB_ENGINE \
+    --master-username $DB_MASTER_USERNAME \
+    --master-user-password $DB_MASTER_PASSWORD \
+    --allocated-storage $DB_ALLOCATED_STORAGE \
+    --vpc-security-group-ids $SECURITY_GROUP_ID \
+    --db-name $DB_NAME \
+    --port $DB_PORT
+
+# Esperar hasta que la instancia esté disponible
+echo "Esperando a que la instancia de la base de datos esté disponible..."
+aws rds wait db-instance-available --db-instance-identifier $DB_INSTANCE_IDENTIFIER
+echo "La instancia de la base de datos está disponible."
+
+# Obtener el endpoint de la instancia de la base de datos
+DB_ENDPOINT=$(aws rds describe-db-instances --db-instance-identifier $DB_INSTANCE_IDENTIFIER --query "DBInstances[0].Endpoint.Address" --output text)
+
+
 
 echo "La instancia de la base de datos ha sido creada y el grupo de seguridad para MySQL Workbench ha sido configurado."
 echo "Endpoint de la base de datos: $DB_ENDPOINT"
